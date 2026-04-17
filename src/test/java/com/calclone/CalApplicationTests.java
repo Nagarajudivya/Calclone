8f548444613bc19620633cc7d0da1b6be237d636
nagarajudivya <nagaraju.divya.36.2@mountblue.tech>
2026-04-17 12:10:04 +0530

    Intial commit

---
+++ src/test/java/com/calclone/CalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
